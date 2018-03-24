56eabc7cbe2798f55a5b8e528d07fe344ca9907b
jonas <jonas_vinck@hotmail.com>
2018-03-24 23:53:55 +0100

    ShortestJobFirst

--- src/practicum1/Simulation/ScheduleAlgorithms/ShortestJobFirst.java
+++ src/practicum1/Simulation/ScheduleAlgorithms/ShortestJobFirst.java
@@ -1,13 +1,74 @@
 package practicum1.Simulation.ScheduleAlgorithms;
 
+import javafx.scene.layout.Priority;
+import practicum1.DataProcessing.Containers.ProcessInfo;
+import practicum1.DataProcessing.Containers.ProcessList;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
 public class ShortestJobFirst implements ProcessAlgorithm{
+
+    private ProcessList backup;
+    private ProcessList processList;
+    private PriorityQueue<ProcessInfo> que;
+    private int elapsedTime;
+
+    public ShortestJobFirst(ProcessList processList) {
+        this.backup = (ProcessList) processList.clone();
+        this.processList = processList;
+        this.elapsedTime = 0;
+        this.que = new PriorityQueue<>(new ShortestFirst());
+    }
+
     @Override
     public void run() {
 
+//        int last = 0;
+
+        while(this.processList.size() != 0 || this.que.size() != 0) {
+
+            if (processList.size() != 0) {
+
+                if (this.que.size() == 0) {
+                    elapsedTime = processList.getFirst().getArrivalTime();
+//                    last = processList.getFirst().getId();
+                }
+
+                if (elapsedTime >= processList.getFirst().getArrivalTime()) {
+                    que.add(processList.removeFirst());
+                }
+            }
+            elapsedTime += que.poll().getServiceTime();
+        }
+/*
+        boolean found = false;
+        int time = 0;
+        for(ProcessInfo info: backup){
+            if(info.getId() == last){
+                time = info.getArrivalTime();
+                found = true;
+            }
+            if(found) time += info.getServiceTime();
+        }
+        System.out.println(time);
+        System.out.println(elapsedTime);
+*/
     }
 
     @Override
     public String getAlgorithmName() {
-        return null;
+        return "shortestjobfirst";
+    }
+
+}
+
+class ShortestFirst implements Comparator<ProcessInfo>{
+
+    @Override
+    public int compare(ProcessInfo service1, ProcessInfo service2){
+
+        return service1.getServiceTime() - service2.getServiceTime();
     }
 }
